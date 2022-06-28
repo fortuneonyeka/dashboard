37c47aec3f9d2c68a1483988d772ea864269bd93
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2022-06-28 13:58:28 +0100

    initial tooltip styling

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ const App = () => {
  <div className="flex relative dark:bg-main-dark-bg">
   <div className="fixed right-4 bottom-4" style={{zIndex:"1000"}}>
     <TooltipComponent content="Settings" postion="Top">
-    <button><FiSettings /></button>
+    <button type="button" className="text-3xl p-3 hover:drops-shadow-xl hover:bg-light-gray text-white" style={{background:"blue", borderRadius:"50%"}}><FiSettings /></button>
     </TooltipComponent>
   </div>
 
