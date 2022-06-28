f493e0eb6dff67716e8c78fc88d65abba688cd43
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2022-06-28 13:34:54 +0100

    import hooks and libraries

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
-
+import React, {useEffect} from "react";
+import {BrowserRouter,Routes,Route} from "react-router-dom"
+import {FiSettings} from "react-icons/fi"
+import {TooltipComponent} from "@syncfusion/ej2-react-popups"
 function App() {
-  return <div className="App"></div>;
+  return <div className="App">
+
+  </div>;
 }
 
 export default App;
