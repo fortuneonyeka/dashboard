7b71ceec77241f223b7694a191b7583439779948
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2022-06-28 14:33:55 +0100

    created sample Routes

--- src/App.js
+++ src/App.js
@@ -32,7 +32,34 @@ const App = () => {
     </div>
   </div>
 
-  
+  <div>
+    <Routes>
+      {/* Dashboard */}
+      <Route path="/" element="ECommerce"/>
+      <Route path="/ecommerce" element="ECommerce"/>
+
+      {/* pages */}
+      <Route path="/orders" element="Orders"/>
+      <Route path="/employees" element="Employees"/>
+      <Route path="/customers" element="Customers"/>
+
+      {/* Apps */}
+      <Route path="/calendar" element="Calendar"/>
+      <Route path="/kanban" element="Kanban"/>
+      <Route path="/editor" element="Editor"/>
+      <Route path="/color-picker" element="Color-Picker"/>
+
+      {/* Charts */}
+      <Route path="/line" element="Line"/>
+      <Route path="/area" element="Area"/>
+      <Route path="/bar" element="Bar"/>
+      <Route path="/pie" element="Pie"/>
+      <Route path="/financial" element="Financial"/>
+      <Route path="/color-mapping" element="Color-Mapping"/>
+      <Route path="/pyramid" element="Pyramid"/>
+
+    </Routes>
+  </div>
  </div>
  </BrowserRouter>
    </div>
